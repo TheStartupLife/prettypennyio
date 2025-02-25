d6b6828537106b960417d99f6333fd9df2e657aa
Whitney <39642663+whitneylubin@users.noreply.github.com>
2025-02-25 23:25:41 +0000

    changed font size for tagline

--- src/components/sections/header.js
+++ src/components/sections/header.js
@@ -1,26 +1,26 @@
 import React from "react"
 import styled from "styled-components"
 import { graphql, useStaticQuery } from "gatsby"
-import Img from "gatsby-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import { Container } from "../global"
 
 const Header = () => {
   const data = useStaticQuery(graphql`
     query {
-            file(relativePath: { eq: "product/green-skew.png" }) {
-              childImageSharp {
-                fluid(maxWidth: 1000) {
-                  ...GatsbyImageSharpFluid_tracedSVG
-                }
-              }
-            }
-          }
-
+      file(relativePath: { eq: "product/green-skew.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 1000, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+        }
+      }
+    }
   `)
+
+  const image = getImage(data.file.childImageSharp)
 
   const handleSubmit = event => {
     event.preventDefault()
+    console.log("Form submitted!") // Optional: Add a console log for debugging
   }
 
   return (
@@ -34,31 +34,31 @@ const Header = () => {
               <br />
               needs more funds
             </h1>
-            <h2>
-              We're building a next generation personal finance tool. Sign up to
+            <p>
+              We're building a next-generation personal finance tool. Sign up to
               get early access.
-            </h2>
+            </p>
             <HeaderForm
-            name="early-access"
-            method="post"
-            data-netlify-honeypot="bot-field"
-            data-netlify="true"
+              name="early-access"
+              method="post"
+              data-netlify-honeypot="bot-field"
+              data-netlify="true"
+              onSubmit={handleSubmit}
             >
               <input type="hidden" name="bot-field" />
               <input type="hidden" name="form-name" value="early-access" />
               <HeaderInput
-              type="email"
-              placeholder="Your email"
-              name="email"
-              id="email"
-              required
+                type="email"
+                placeholder="Your email"
+                name="email"
+                id="email"
+                required
               />
-            <HeaderButton>Early access</HeaderButton>
+              <HeaderButton type="submit">Early access</HeaderButton>
             </HeaderForm>
           </HeaderTextGroup>
           <ImageWrapper>
-            <StyledImage fluid={data.file.childImageSharp.fluid} />
-            <br />
+            <StyledImage image={image} alt="Green Skew Product Image" />
           </ImageWrapper>
         </Flex>
       </Container>
@@ -73,13 +73,11 @@ const HeaderWrapper = styled.header`
   padding: 160px 0 80px 0;
   position: relative;
   clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 5vw));
-  @media (max-width: ${props => props.theme.screen.md}) {
-  }
 `
+
 const Subtitle = styled.h5`
   font-size: 16px;
   color: ${props => props.theme.color.accent};
-  letter-spacing: 0px;
   margin-bottom: 16px;
 `
 
@@ -100,13 +98,8 @@ const HeaderTextGroup = styled.div`
     color: ${props => props.theme.color.primary};
   }
 
-  h2 {
-    margin-bottom: 24px;
-    ${props => props.theme.font_size.regular}
-  }
-
   p {
-    margin-bottom: 48px;
+    margin-bottom: 24px;
   }
 `
 
@@ -131,18 +124,6 @@ const HeaderForm = styled.form`
   }
 `
 
-// const FormSubtitle = styled.span`
-//   ${props => props.theme.font_size.xxsmall}
-// `
-
-// const FormSubtitleLink = styled(Link)`
-//   color: ${props => props.theme.color.secondary};
-//   padding-bottom: 1px;
-//   margin-left: 8px;
-//   text-decoration: none;
-//   border-bottom: 1px solid ${props => props.theme.color.secondary};
-// `
-
 const HeaderInput = styled.input`
   font-weight: 500;
   font-size: 16px;
@@ -151,22 +132,13 @@ const HeaderInput = styled.input`
   width: 100%;
   text-align: left;
   height: 60px;
-  border-width: 1px;
-  border-style: solid;
-  border-color: ${props => props.theme.color.secondary};
-  border-image: initial;
+  border: 1px solid ${props => props.theme.color.secondary};
   border-radius: 4px;
   padding: 8px 16px;
-  outline: 0px;
+  outline: none;
+
   &:focus {
     box-shadow: inset ${props => props.theme.color.secondary} 0px 0px 0px 2px;
-  }
-  @media (max-width: ${props => props.theme.screen.md}) {
-    margin-bottom: 8px;
-  }
-  @media (max-width: ${props => props.theme.screen.sm}) {
-    display: block;
-    width: 100%;
   }
 `
 
@@ -174,30 +146,25 @@ const HeaderButton = styled.button`
   font-weight: 500;
   font-size: 14px;
   color: white;
-  letter-spacing: 1px;
   height: 60px;
-  display: block;
   margin-left: 8px;
   text-transform: uppercase;
   cursor: pointer;
-  white-space: nowrap;
   background: ${props => props.theme.color.secondary};
   border-radius: 4px;
   padding: 0px 40px;
-  border-width: 0px;
-  border-style: initial;
-  border-color: initial;
-  border-image: initial;
-  outline: 0px;
+  border: none;
+  outline: none;
+
   &:hover {
     box-shadow: rgba(110, 120, 152, 0.22) 0px 2px 10px 0px;
   }
-  @media (max-width: ${props => props.theme.screen.md}) {
-  }
+
   @media (max-width: ${props => props.theme.screen.sm}) {
     margin-left: 0;
   }
 `
+
 const ImageWrapper = styled.div`
   justify-self: end;
   align-self: center;
@@ -206,7 +173,7 @@ const ImageWrapper = styled.div`
   }
 `
 
-const StyledImage = styled(Img)`
+const StyledImage = styled(GatsbyImage)`
   width: 500px;
   @media (max-width: ${props => props.theme.screen.md}) {
     width: 400px;
